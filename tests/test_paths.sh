5d625d7f56aa0157b84de3a6a3b94b5e1c33f568
Hamed Haghjo <hamedhaghjo@hotmail.com>
2024-11-14 13:00:26 +0100

    fix: Restore shellcheck directive in test paths

--- tests/test_paths.sh
+++ tests/test_paths.sh
@@ -4,6 +4,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 declare -A RUNTIME_DIRS
+# shellcheck disable=SC2034  # TRASH_DIRS is used in sourced files
 declare -A TRASH_DIRS
 
 source "${SCRIPT_DIR}/../src/lib/logger.sh"
